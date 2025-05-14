a3a9db4d1dcefd605bb7b18c46e84463a9e614f6
Tran Dinh Son <sontran12242004@gmail.com>
2025-05-14 21:37:00 +0700

    Add files via upload

---
+++ SCSP_G6/src/main/java/com/smokingcessation/scsp_g6/ScspG6Application.java
@@ -0,0 +1,13 @@
+package com.smokingcessation.scsp_g6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScspG6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScspG6Application.class, args);
+    }
+
+}
---
+++ SCSP_G6/src/test/java/com/smokingcessation/scsp_g6/ScspG6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smokingcessation.scsp_g6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScspG6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
